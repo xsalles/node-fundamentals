9a629829ffd538fab8c26746b67b9679dff5fced
xsalles <xs.salles@gmail.com>
2025-01-26 12:53:23 -0300

    feat: created a user using method POST and learned good practices of header.

--- src/server.js
+++ src/server.js
@@ -1,31 +1,25 @@
 import http from 'node:http'
 
-// By req we can create a user using name, email, password
-// By res we can return a answer to the user
-
-// Routes are like enter points that are provide for the user
-
-// HTTP has a two main points that are method HTTP that is used and the URL 
-
-// GET, POST, PÚT, PATCH, DELETE that are more used in day to day
-
-// GET - search a resource from backend
-// POST - create a new resource
-// PUT - update a resource from backend
-// PATCH - update a specific information from a resource at backend (accept notifications or no)
-// DELETE - delete a resource from backend
-
-
+const users = []
 
 const server = http.createServer((req, res) => {
    const { method, url } = req;
 
    if (method === 'GET' && url === '/users') {
-    return res.end("user list")
+
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
    }
 
    if (method === 'POST' && url === '/users') {
-    return res.end("creating user")
+    users.push({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      id: 1
+    })
+
+    return res.end('User created')
    }
 
    return res.end('Hello World')
